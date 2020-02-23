a6bd3c869f934f5866360cd0a188988f54d058d6
prabhat pankaj <prabhatiitbhu@gmail.com>
2020-02-23 10:23:29 +0530

    final

---
+++ lambda/example/1.nodejs/lambda.tf
@@ -0,0 +1,9 @@
+# Create the function
+resource "aws_lambda_function" "service" {
+  filename         = "lambda_function.zip"
+  function_name    = "test_lambda"
+  role             = "${aws_iam_role.iam_for_lambda_tf.arn}"
+  handler          = "index.handler"
+  source_code_hash = "${data.archive_file.lambda_zip.output_base64sha256}"
+  runtime          = "nodejs10.x"
+}
